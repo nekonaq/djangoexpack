88c000c1b8639b68bf00c6e0046eba7faec1ba0a
Tatsuo Nakajyo <tnak@nekonaq.com>
2021-08-02 11:11:44 +0000

    remove utils.settings.detect_settings_module()

--- djangoexpack/utils/settings.py
+++ djangoexpack/utils/settings.py
@@ -2,17 +2,11 @@ import os
 from pyhiera import Hiera
 
 
-def detect_settings_module(base_dir):
-    envpath = os.path.realpath(os.path.join(base_dir, 'secrets/environments/default'))
-    app_environ = os.path.basename(envpath) if os.path.exists(envpath) else 'local'
-    return 'siteconf.settings.{}'.format(app_environ)
-
-
 def load_settings_from_config(hiera_configs, **context):
     for confpath in hiera_configs if isinstance(hiera_configs, (list, tuple)) else (hiera_configs,):
         if not os.path.exists(confpath):
             continue
-        print("# config:", confpath)
+        # print("# config:", confpath)
         hiera = Hiera.load_data(confpath, context=context)
         hiera_data = hiera.flatten()
         return hiera_data
