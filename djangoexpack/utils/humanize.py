971304f66f8c9e2ab73933f26f80398b0c25e492
Tatsuo Nakajyo <tnak@nekonaq.com>
2021-09-22 15:35:29 +0000

    add utils.humanize.NaturalTimeJaFormatter

---
+++ djangoexpack/utils/humanize.py
@@ -0,0 +1,8 @@
+from django.contrib.humanize.templatetags.humanize import NaturalTimeFormatter
+
+
+class NaturalTimeJaFormatter(NaturalTimeFormatter):
+    @classmethod
+    def string_for(cls, value):
+        ret = super().string_for(value)
+        return 'と'.join(ret.split(', '))
